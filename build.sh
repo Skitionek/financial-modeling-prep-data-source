0d522fc2d95d155d26080ab72a5f3ad2903241a7
Dominik Maszczyk <Skitionek@gmail.com>
2020-04-23 13:42:43 +0200

    bump version based on snapshots

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#author     :Dominik Maszczyk
-#date       :2019-06-12
-#email      :Skitionek@gmail.com
-#==============================================================================
-
-cross-env BABEL_ENV=production babel src --out-dir ./ --source-maps --ignore "*.json" && (echo "Build successful")
